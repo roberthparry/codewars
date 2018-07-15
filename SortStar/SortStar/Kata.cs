63d06f0117ddacf27d5abeb5721d72e4fb491f8a
Robert Parry <robert.h.parry@gmail.com>
2018-07-15 23:47:21 +0100

    Added SortStar

---
+++ SortStar/SortStar/Kata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string TwoSort(string[] s)
+    {
+        Array.Sort(s, StringComparer.Ordinal);
+        return string.Join("***", s[0].Select(c => $"{c}").ToArray());
+    }
+}
---
+++ SortStar/SortStar/Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace Solution
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("b***i***t***c***o***i***n", Kata.TwoSort(new[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" }));
+            Assert.AreEqual("a***r***e", Kata.TwoSort(new[] { "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones" }));
+        }
+    }
+}
