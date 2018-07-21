f59398018bd477c44c466ecbcc541f8c6a591ac2
Robert Parry <robert.h.parry@gmail.com>
2018-07-21 16:30:53 +0100

    added pipi numbers kata

---
+++ src/Kata/PipiNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using System.Collections.Generic;
+
+namespace Kata 
+{
+    public class PipiNumbers
+    {
+        private static Dictionary<int, BigInteger> pipiNumberDict = new Dictionary<int, BigInteger>{
+        {0, 0}
+    };
+
+        public static BigInteger Pipi(int n)
+        {
+            BigInteger p;
+            if (!pipiNumberDict.TryGetValue(n, out p))
+            {
+                if (n == 0) return new BigInteger(0);
+                p = new BigInteger(n);
+                for (int i = 0; i < n; i++)
+                {
+                    p -= Pipi(i);
+                    p *= p;
+                }
+                pipiNumberDict[n] = p;
+            }
+
+            return p;
+        }
+    } 
+}
