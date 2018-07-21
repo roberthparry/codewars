976e2d7080b1c2d4d33c8bcaea76f9eaf0b0ed15
Robert Parry <robert.h.parry@gmail.com>
2018-07-21 18:23:19 +0100

    add LinearSystem and nuget package MathNet.Numerics

---
+++ src/Kata/LinearSystem.cs
@@ -0,0 +1,46 @@
+﻿namespace LinearSystems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using MathNet.Numerics.LinearAlgebra.Double;
+
+    public class LinearSystem
+    {
+
+        public string Solve(string input)
+        {
+            string[] lines = Regex.Split(input, "\r\n|\r|\n");
+            double[][] table = lines.Select(l => Regex.Split(l, @"\s+")
+                                                      .Where(s => s != string.Empty)
+                                                      .Select(t => double.Parse(t))
+                                                      .ToArray()).ToArray();
+
+            var ylist = new List<double>();
+            var mlistlist = new List<List<double>>();
+            foreach (double[] row in table)
+            {
+                ylist.Add(row[row.Length - 1]);
+                var mlist = new List<double>();
+                for (int i = 0; i < row.Length - 1; i++)
+                    mlist.Add(row[i]);
+                mlistlist.Add(mlist);
+            }
+
+            double[] y = ylist.ToArray();
+            double[,] m = new double[mlistlist.Count, mlistlist[0].Count];
+            for (int i = 0; i < mlistlist.Count; i++)
+                for (int j = 0; j < mlistlist[i].Count; j++)
+                    m[i, j] = mlistlist[i][j];
+
+            var matrix = DenseMatrix.OfArray(m);
+            if (Math.Abs(matrix.Determinant()) < 1.0e-12)
+                return "SOLUTION=NONE";
+
+            var vector = DenseVector.OfArray(y);
+            var result = matrix.Inverse().Multiply(vector);
+            return $"SOLUTION=({string.Join("; ", result.Select(e => $"{e}"))})";
+        }
+    }
+}
---
+++ src/KataTests/LinearSystemTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LinearSystems
+{
+    using NUnit.Framework;
+    using System;
+    [TestFixture]
+
+    public class SolvingTests
+    {
+        [Test]
+        public void TestAndVerify1()
+        {
+            LinearSystem ls = new LinearSystem();
+            string input = "1 2 0 7\r\n0 2 4 8\r\n0 5 6 9";
+            string result = ls.Solve(input);
+            Assert.AreEqual("SOLUTION=(10; -1.5; 2.75)", result);
+        }
+        [Test]
+        public void TestAndVerify2()
+        {
+            LinearSystem ls = new LinearSystem();
+            string input = "1 2 0 4 7\r\n0 2 0 2 8\r\n0 0 -1 4 6\r\n1 2 3 2 3";
+            string result = ls.Solve(input);
+            Assert.AreEqual("SOLUTION=(-3.8; 2.6; -0.4; 1.4)", result);
+        }
+
+        [Test]
+        public void TestAndVerify3()
+        {
+            LinearSystem ls = new LinearSystem();
+            string input = "1 2 1\r\n1 2 0";
+            string result = ls.Solve(input);
+            Assert.AreEqual("SOLUTION=NONE", result);
+        }
+    }
+}
