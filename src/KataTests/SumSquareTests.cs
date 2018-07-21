b276258df1ae263b1db52f842a233267210e67e4
Robert Parry <robert.h.parry@gmail.com>
2018-07-21 13:28:17 +0100

    fix namespace names and class names

--- src/KataTests/SumSquareTests.cs
+++ src/KataTests/SumSquareTests.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections.Generic;
 using NUnit.Framework;
+using Kata;
 
-[TestFixture]
-public class Tests
+namespace KataTests 
 {
-    private static IEnumerable<TestCaseData> sampleTestCases
-    {
-        get
-        {
-            yield return new TestCaseData(new int[] { 1, 2, 2 }).Returns(9);
-            yield return new TestCaseData(new int[] { 1, 2 }).Returns(5);
-            yield return new TestCaseData(new int[] { 5, 3, 4 }).Returns(50);
-        }
-    }
 
-    [Test, TestCaseSource("sampleTestCases"), Description("Sample Tests")]
-    public int SampleTest(int[] n) => SumSquare.SquareSum(n);
+    [TestFixture]
+    public class SumSquareTests
+    {
+        private static IEnumerable<TestCaseData> sampleTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 1, 2, 2 }).Returns(9);
+                yield return new TestCaseData(new int[] { 1, 2 }).Returns(5);
+                yield return new TestCaseData(new int[] { 5, 3, 4 }).Returns(50);
+            }
+        }
+
+        [Test, TestCaseSource("sampleTestCases"), Description("Sample Tests")]
+        public int SampleTest(int[] n) => SumSquare.SquareSum(n);
+    } 
 }
