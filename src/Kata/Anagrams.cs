6ad6e4e393fb5db136069b42520508f6ea716855
roberthparry <robert.h.parry@gmail.com>
2018-07-22 14:16:35 +0100

    add Anagrams

---
+++ src/Kata/Anagrams.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Kata
+{
+	public static class Anagrams
+    {
+        private static long[] primes = new long[] {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101
+        };
+
+        public static List<string> FindAnagrams(string word, List<string> words)
+        {
+            var dict = word.Distinct()
+			               .Select((ch, i) => new { Key = char.ToLower(ch), Value = primes[i] })
+			               .ToDictionary(o => o.Key, o => o.Value);
+			BigInteger encodedWord = Encode(word, dict);
+			return words.Where(w => Encode(w, dict) == encodedWord).ToList();
+        }
+
+        private static BigInteger Encode(string word, Dictionary<char, long> primeLookup)
+        {
+            var encoded = new BigInteger(1);
+            foreach (char ch in word)
+            {
+				long code;
+				if (!primeLookup.TryGetValue(char.ToLower(ch), out code))
+					return new BigInteger(-1);
+				
+                encoded *= code;
+            }
+            return encoded;
+        }
+    }	
+}
---
+++ src/KataTests/AnagramsTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using Kata;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class AnagramsTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new List<string> { "a" }, Anagrams.FindAnagrams("a", new List<string> { "a", "b", "c", "d" }));
+			Assert.AreEqual(new List<string> { "carer", "arcre", "carre" }, Anagrams.FindAnagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));
+        }
+    }
+}
