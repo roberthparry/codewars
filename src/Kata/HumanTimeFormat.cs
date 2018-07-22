830f15a0616826cc0d634255fea2b8788b418539
Robert Parry <robert.h.parry@gmail.com>
2018-07-22 02:08:32 +0100

    add HumanTimeFormat with failing tests

---
+++ src/Kata/HumanTimeFormat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata
+{
+    public class HumanTimeFormat
+    {
+        public static string formatDuration(int seconds)
+        {
+            Console.WriteLine($"seconds = {seconds}");
+            if (seconds == 0) return "now";
+
+            var now = DateTime.Now;
+            var later = now.AddSeconds(seconds);
+
+            Console.WriteLine($"now: {now:yyyy-MM-dd hh:mm:ss}, later: {later:yyyy-MM-dd hh:mm:ss}");
+
+            int nowDoy = now.DayOfYear;
+            int laterDoy = later.DayOfYear;
+            int nowYear = now.Year;
+            int laterYear = later.Year;
+
+            int years = laterYear - nowYear;
+            int days = laterDoy - nowDoy;
+            if (laterDoy < nowDoy)
+            {
+                years--;
+                DateTime thisTimeLater = now.AddYears(years);
+                days = (later - thisTimeLater).Days;
+            }
+
+            int nowHour = now.Hour;
+            int laterHour = later.Hour;
+            if (laterHour < nowHour)
+            {
+                days--;
+                laterHour += 24;
+            }
+
+            int nowMinute = now.Minute;
+            int laterMinute = later.Minute;
+            int hours = laterHour - nowHour;
+            if (laterMinute < nowMinute)
+            {
+                hours--;
+                laterMinute += 60;
+            }
+
+            int nowSecond = now.Second;
+            int laterSecond = later.Second;
+            int minutes = laterMinute - nowMinute;
+            if (laterSecond < nowSecond)
+            {
+                minutes--;
+                laterSecond += 60;
+            }
+
+            int secs = laterSecond - nowSecond;
+
+            var output = new List<string>();
+
+            if (years > 0) output.Add((years > 1) ? $"{years} years" : $"{years} year");
+            if (days > 0) output.Add((days > 1) ? $"{days} days" : $"{days} day");
+            if (hours > 0) output.Add((hours > 1) ? $"{hours} hours" : $"{hours} hour");
+            if (minutes > 0) output.Add((minutes > 1) ? $"{minutes} minutes" : $"{minutes} minute");
+            if (secs > 0) output.Add((secs > 1) ? $"{secs} seconds" : $"{secs} second");
+
+            string final = output.Count > 1 ? $" and {output[output.Count - 1]}" : output[0];
+            string result = $"{string.Join(", ", output.GetRange(0, output.Count - 1))}{final}";
+            return result;
+        }
+    }    
+}
---
+++ src/KataTests/HumanTimeFormatTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class HumanTimeFormatTests
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual("now", HumanTimeFormat.formatDuration(0));
+        }
+
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual("1 second", HumanTimeFormat.formatDuration(1));
+        }
+
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual("1 minute and 2 seconds", HumanTimeFormat.formatDuration(62));
+        }
+
+        [Test]
+        public void test4()
+        {
+            Assert.AreEqual("2 minutes", HumanTimeFormat.formatDuration(120));
+        }
+
+        [Test]
+        public void test5()
+        {
+            Assert.AreEqual("1 hour, 1 minute and 2 seconds", HumanTimeFormat.formatDuration(3662));
+        }
+
+        [Test]
+        public void test6()
+        {
+            Assert.AreEqual("7 years, 246 days, 15 hours, 32 minutes and 54 seconds", HumanTimeFormat.formatDuration(242062374));
+        }
+    } 
+}
+
+
