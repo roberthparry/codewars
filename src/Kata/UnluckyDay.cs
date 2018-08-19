eca6439cc2febef94540934f4e25a17be5b75945
roberthparry <robert.h.parry@gmail.com>
2018-08-19 21:36:11 +0100

    add UnluckyDay

---
+++ src/Kata/UnluckyDay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata
+{
+	public static class UnluckyDay
+	{
+		public static int UnluckyDays(int year)
+		{
+			return Enumerable.Range(1, 12).Count(i => new DateTime(year, i, 13).DayOfWeek == DayOfWeek.Friday);
+		}
+	}
+}
