c297b8c5bd13fd51797e6f7a418000da276b2547
roberthparry <robert.h.parry@gmail.com>
2018-08-19 13:11:24 +0100

    added ReverseNumbers

---
+++ src/Kata/ReverseNumbers.cs
@@ -0,0 +1,11 @@
+﻿namespace Kata
+{
+    public class ReverseNumbers
+    {
+        public int ReverseNumber(int n)
+        {
+            int Reverse(int m, int rev) => (m == 0) ? rev : Reverse(m / 10, rev * 10 + m % 10);
+            return Reverse(n, 0);
+        }
+    }
+}
