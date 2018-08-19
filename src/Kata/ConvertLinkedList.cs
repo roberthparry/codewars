6298811d5aaf25fe5fafc6ffe8f7dfcae4d43b18
roberthparry <robert.h.parry@gmail.com>
2018-08-19 17:07:32 +0100

    add ConvertLinkList

---
+++ src/Kata/ConvertLinkedList.cs
@@ -0,0 +1,19 @@
+﻿namespace Kata
+{
+	public class Node
+	{
+		public int Data { get; private set; }
+		public Node Next { get; private set; }
+
+		public Node(int data, Node next = null)
+		{
+			Data = data;
+			Next = next;
+		}
+	}
+
+	public static class ConvertLinkedList
+	{
+		public static string Stringify(Node list) => (list == null) ? "null" : $"{list.Data} -> {Stringify(list.Next)}";
+	}
+}
