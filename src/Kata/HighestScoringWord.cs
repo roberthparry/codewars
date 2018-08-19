c8735e48498af0a39ab6aa9a9c32b119d10a3d1d
roberthparry <robert.h.parry@gmail.com>
2018-08-19 15:28:52 +0100

    added HighestScoringWord and HighestScoringWWordTests

---
+++ src/Kata/HighestScoringWord.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Kata
+{
+	public static class HighestScoringWord
+	{
+		public static string High(string s)
+		{
+			int maxScorePos = -1;
+			int maxScore = -1;
+			string[] words = s.Split(' ');
+			for (int i = 0; i < words.Length; i++)
+			{
+				string word = words[i];
+				int score = word.Select(ch => (int)(ch - 'a')).Sum();
+				if (score > maxScore)
+				{
+					maxScore = score;
+					maxScorePos = i;
+				}
+			}
+			return words[maxScorePos];
+		}
+	}
+}
