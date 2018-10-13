6ea56ad83cb924705e62ca1321484be7fe0308c8
roberthparry <robert.h.parry@gmail.com>
2018-10-13 18:31:59 +0100

    two more Katas with tests

---
+++ src/Kata/IntegerDepth.cs
@@ -0,0 +1,28 @@
+﻿namespace Kata
+{
+    public class IntegerDepth
+    {
+        public static void AddToSet(int n, ref int set)
+        {
+            set |= 1 << (n % 10);
+
+            if (n < 10)
+                return;
+
+            AddToSet(n / 10, ref set);
+        }
+
+        public static int ComputeDepth(int n)
+        {
+            int set = 0;
+            int depth = 0;
+            do
+            {
+                depth++;
+                AddToSet(n * depth, ref set);
+            } while (set < 1023);
+            return depth;
+        }
+
+    }
+}
---
+++ src/Kata/MyJinxin.cs
@@ -0,0 +1,11 @@
+﻿namespace Kata
+{
+    public class MyJinxin
+    {
+        public string ReverseLetter(string str)
+        {
+            string Letter(char ch) => char.IsLetter(ch) ? $"{ch}" : "";
+            return string.IsNullOrEmpty(str) ? str : $"{ReverseLetter(str.Substring(1))}{Letter(str[0])}";
+        }
+    }
+}
