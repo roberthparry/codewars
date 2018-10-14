04191166c7f3574c544f46ecf5f734d107fd78af
roberthparry <robert.h.parry@gmail.com>
2018-10-14 14:06:58 +0100

    Added ArrayDifference Kata

---
+++ src/Kata/ArrayDifference.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Kata
+{
+    public static class ArrayDifference
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            return a.Where(i => !b.Contains(i)).ToArray();
+        }
+    }
+}
