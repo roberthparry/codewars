370bad54b27349568777e5fa7d0e4e521dc9d16d
roberthparry <robert.h.parry@gmail.com>
2018-10-15 01:56:17 +0100

    add alternative FindDigit()

--- src/KataTests/NthDigitTests.cs
+++ src/KataTests/NthDigitTests.cs
@@ -19,5 +19,17 @@ namespace KataTests
             Assert.AreEqual(-1, NthDigit.FindDigit(65, 0));
             Assert.AreEqual(-1, NthDigit.FindDigit(24, -8));
         }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(5, NthDigit.FindDigit2(5673, 4));
+            Assert.AreEqual(2, NthDigit.FindDigit2(129, 2));
+            Assert.AreEqual(8, NthDigit.FindDigit2(-2825, 3));
+            Assert.AreEqual(0, NthDigit.FindDigit2(-456, 4));
+            Assert.AreEqual(0, NthDigit.FindDigit2(0, 20));
+            Assert.AreEqual(-1, NthDigit.FindDigit2(65, 0));
+            Assert.AreEqual(-1, NthDigit.FindDigit2(24, -8));
+        }
     }
 }
