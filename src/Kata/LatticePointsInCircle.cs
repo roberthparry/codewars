2c5a8f411c00c74aa3f03fb895c2ac9e1ba492fc
roberthparry <robert.h.parry@gmail.com>
2018-11-08 16:45:25 +0000

    Add LatticePointsInCircle

---
+++ src/Kata/LatticePointsInCircle.cs
@@ -0,0 +1,23 @@
+﻿namespace Kata
+{
+    public static class LatticePointsInCircle
+    {
+        public static int PointsInCircle(int radius)
+        {
+            int r2 = radius * radius;
+            int count = radius;
+            for (int i = 1; i <= radius; i++)
+            {
+                int i2 = i * i;
+                for (int j = 1; j <= radius; j++)
+                {
+                    if (i2 + j * j > r2) break;
+                    count++;
+                }
+            }
+
+            return count * 4 + 1;
+        }
+    }
+}
+
