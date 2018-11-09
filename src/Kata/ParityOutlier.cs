957d73f446c12b8a568ca45927d5da15c79584f8
roberthparry <robert.h.parry@gmail.com>
2018-11-09 15:06:12 +0000

    Add ParityOutlier

---
+++ src/Kata/ParityOutlier.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace Kata
+{
+    public static class ParityOutlier
+    {
+        public static int Find(int[] integers)
+        {
+            bool odds = false, evens = false;
+            int lastOdd = 0, lastEven = 0;
+            foreach (int i in integers)
+            {
+                if (i % 2 == 0)
+                {
+                    lastEven = i;
+                    if (evens && odds)
+                        return lastOdd;
+                    evens = true;
+                }
+                else
+                {
+                    lastOdd = i;
+                    if (odds && evens)
+                        return lastEven;
+                    odds = true;
+                }
+            }
+            return integers.Last();
+        }
+    }
+}
