ee44141dfaebaecbc9ddb02dbc33f83616816618
roberthparry <robert.h.parry@gmail.com>
2018-11-27 03:27:46 +0000

    Add MeanSquareError

---
+++ src/Kata/MeanSquareError.cs
@@ -0,0 +1,16 @@
+﻿namespace Kata
+{
+    public static class MeanSquareError
+    {
+        public static double Solution(int[] firstArray, int[] secondArray)
+        {
+            double total = 0.0;
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                double d = firstArray[i] - secondArray[i];
+                total += d * d;
+            }
+            return total / firstArray.Length;
+        }
+    }
+}
---
+++ src/KataTests/MeanSquareErrorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class MeanSquareErrorTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(9, MeanSquareError.Solution(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+            Assert.AreEqual(16.5, MeanSquareError.Solution(new int[] { 10, 20, 10, 2 }, new int[] { 10, 25, 5, -2 }));
+            Assert.AreEqual(1, MeanSquareError.Solution(new int[] { 0, -1 }, new int[] { -1, 0 }));
+        }
+    }
+}
