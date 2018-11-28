c59338b40c0f5982b08e7ae45aa33a43a159a95d
roberthparry <robert.h.parry@gmail.com>
2018-11-28 20:40:30 +0000

    Add CountPosSumNeg and SumFractions

---
+++ src/Kata/CountPosSumNeg.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Kata
+{
+    public static class CountPosSumNeg
+    {
+        public static int[] CountPositivesSumNegatives(int[] input) =>
+            (input == null || input.Length == 0) ? new int[0] :
+                input.Aggregate(new int[] { 0, 0 }, (acc, e) => {
+                    if (e > 0) acc[0]++;
+                    else if (e < 0) acc[1] += e;
+                    return acc;
+                });
+    }
+}
---
+++ src/KataTests/CountPosSumNegTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class CountPosSumNegTests
+    {
+        [Test]
+        public void CountPositivesSumNegatives_BasicTest()
+        {
+            int[] expectedResult = new int[] { 10, -65 };
+
+            Assert.AreEqual(expectedResult, CountPosSumNeg.CountPositivesSumNegatives(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 }));
+        }
+
+        [Test]
+        public void CountPositivesSumNegatives_InputWithZeroes()
+        {
+            int[] expectedResult = new int[] { 8, -50 };
+
+            Assert.AreEqual(expectedResult, CountPosSumNeg.CountPositivesSumNegatives(new[] { 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14 }));
+        }
+    }
+}
