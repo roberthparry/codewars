e4a3dca4f8e349afd76eb437f0cffec387e420ce
roberthparry <robert.h.parry@gmail.com>
2018-12-16 22:52:57 +0000

    add IntegerSquareRoot and tests

---
+++ src/Kata/IntegerSquareRoot.cs
@@ -0,0 +1,17 @@
+﻿namespace Kata
+{
+    public static class IntegerSquareRoot
+    {
+        public static object SqrtApproximation(int number)
+        {
+            if (number == 0 || number == 1) return number;
+            int s = number / 2;
+            while (s * s > number)
+                s = (s + number / s) / 2;
+
+            if (s * s < number && number < (s + 1) * (s + 1))
+                return new int[] { s, s + 1 };
+            return s;
+        }
+    }
+}
---
+++ src/KataTests/IntegerSquareRootTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class IntegerSquareRootTests
+    {
+        [TestCase(3, new int[] { 1, 2 })]
+        [TestCase(4, 2)]
+        [TestCase(10, new int[] { 3, 4 })]
+        [TestCase(16, 4)]
+        [TestCase(25, 5)]
+        [TestCase(30, new int[] { 5, 6 })]
+        [TestCase(40, new int[] { 6, 7 })]
+        [TestCase(625, 25)]
+        [TestCase(626, new int[] { 25, 26 })]
+        [TestCase(624, new int[] { 24, 25 })]
+        public void IntegerSqrt_Test(int number, object expected)
+        {
+            Assert.AreEqual(expected, IntegerSquareRoot.SqrtApproximation(number));
+        }
+    }
+}
